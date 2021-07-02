dc7dda3da82163f9fdcba751b223453b454a2a31
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2021-07-02 16:13:14 -0300

    ok

--- main.js
+++ main.js
@@ -1,122 +1,84 @@
-var id = localStorage['count'] || 0;
-id = JSON.parse(id)
+var tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
-function changeState(content, state) {
-
-    let Array = JSON.parse(localStorage['tasks'])
-    Array.forEach((a) => {
-        if (a.texto === content) {
-            a.state = state
-            localStorage.setItem('tasks', JSON.stringify(Array))
-            return
-
-        }
-
-
-    })
-}
-
-function SaveArrayDatas(KeytoSave, Content) {
-
-    if (localStorage[KeytoSave] === undefined) {
-        let Array = [Content]
-        localStorage.setItem(KeytoSave, JSON.stringify(Array))
-        return
-    }
-    let Array = JSON.parse(localStorage[KeytoSave])
-    Array.push(Content)
-    localStorage.setItem(KeytoSave, JSON.stringify(Array))
-
-}
-
-
-function addtask(content, ischecked, count) {
-    let checkstate = ischecked || false
-    let ul = document.getElementById('tarefas');
-    let li = document.createElement('li')
-    let delbutton = document.createElement('button')
-    delbutton.setAttribute('class', 'DelButonn')
-    delbutton.setAttribute('onclick', `DeleteTask(${count})`)
-    delbutton.onclick = function() { DeleteTask(count) }
-    delbutton.innerHTML = '&#10006;'
-    let checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.checked = checkstate
-    checkbox.onclick = function() { changeState(content, checkbox.checked) };
-    li.setAttribute("id", `${count}`)
-    li.append(checkbox)
-    li.append(content)
-    li.append(delbutton)
-    ul.append(li)
-
-}
-
-function DeleteTask(count) {
-
-    let Array = JSON.parse(localStorage['tasks'])
-    Array.forEach((a) => {
-        if (a.id === count) {
-            if (count === 1) return Array.shift()
-            return Array.splice(count);
-        }
-    })
-    if (Array.length === 0) localStorage.setItem('count', JSON.stringify(0))
-    localStorage.setItem('tasks', JSON.stringify(Array))
-    listarTarefas()
-
-}
+var count = localStorage.getItem('counter') || 0;
 
 function listarTarefas() {
-    let ul = document.getElementById('tarefas')
+    var ul = document.getElementById('tarefas');
     ul.innerHTML = ""
-    if (localStorage['tasks'] === undefined) return
-    let Array = JSON.parse(localStorage['tasks'])
-    Array.forEach((a) => {
-        addtask(a.texto, a.state, a.id);
+    tarefas.forEach((t) => {
+        if (!t.arquivada) {
+            let li = document.createElement('li')
+            li.setAttribute('id', t.id)
+            let check = document.createElement('input')
+            check.setAttribute('type', 'checkbox')
+            check.onclick = function() {
+                markAsComplete(t.id)
+            }
+            check.checked = t.concluida;
+            if (t.concluida) {
+                li.classList.add('concluida')
+            }
+            let button = document.createElement('button')
+            button.classList.add('arquivar')
+            button.innerHTML = '&#10005;'
 
+            button.onclick = function() {
+                markAsArchived(t.id)
+            }
+            li.append(check)
 
+            let p = document.createElement('p')
+            p.innerText = t.texto
+            li.append(p)
+
+            li.append(button)
+            ul.append(li)
+        }
     })
 }
 
-function ButtonaddTask() {
-    let input = document.getElementById("newTask").value;
-    if (input == "") return alert("Tarefa vazia !!!")
-    if (localStorage['tasks'] === undefined) {
-        let Array = [{
-            texto: input,
-            state: false,
-            arquivada: false,
-            id: id
-        }]
-        id = +1
-        localStorage.setItem('count', JSON.stringify(id))
-        localStorage.setItem('tasks', JSON.stringify(Array))
-        addtask(input, undefined, id)
-        document.getElementById("newTask").value = ""
-        return
+function markAsComplete(idTarefa) {
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].id === idTarefa) {
+            tarefas[i].concluida = !tarefas[i].concluida;
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+            listarTarefas()
+        }
     }
-    let Array = JSON.parse(localStorage['tasks'])
-    Array.push({
-        texto: input,
-        state: false,
-        arquivada: false,
-        id: id
-    })
-    id += 1
-    localStorage.setItem('count', JSON.stringify(id))
-    localStorage.setItem('tasks', JSON.stringify(Array))
-    addtask(input, undefined, id)
-    document.getElementById("newTask").value = ""
-    return
+}
+
+function markAsArchived(idTarefa) {
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].id === idTarefa) {
+            tarefas[i].arquivada = !tarefas[i].arquivada;
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+            listarTarefas()
+        }
+    }
+}
+
+function adicionarTarefa() {
+    let input = prompt("Tarefa");
+    if (input !== null & input !== "") {
+        tarefas.push({
+            texto: input,
+            concluida: false,
+            arquivada: false,
+            id: count
+        })
+        count++
+        localStorage.setItem('counter', count)
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+        listarTarefas()
+    }
 }
 
 function docReady(fn) {
-    // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
         setTimeout(fn, 1);
     } else {
         document.addEventListener("DOMContentLoaded", fn);
     }
 }
+
 docReady(listarTarefas)
